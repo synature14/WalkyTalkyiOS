da608ea8c5698c76bb7ec26cf751847c02c7e862
suyeon <synature14@gmail.com>
2018-10-08 14:37:09 +0900

    AVFoundation 추가, 녹음 기능 추가

--- Walkytalky/MainViewController.swift
+++ Walkytalky/MainViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxGesture
+import AVFoundation
 
-
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, AVAudioRecorderDelegate {
     
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var numberOfRecords: Int = 0
     
     @IBOutlet weak var recordBackColoredView: UIView!
     @IBOutlet weak var recordBtnBackView: UIView!
@@ -23,8 +27,26 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudio()
         setUI()
-        recordButton.rx.tap
+        
+        recordButton.rx
+            .longPressGesture()
+            .when(UIGestureRecognizer.State.changed)
+            .subscribe({ _ in
+                self.recordBackColoredView.backgroundColor = UIColor.red
+                self.startToRecord()
+            })
+            .disposed(by: disposeBag)
+        
+        recordButton.rx
+            .longPressGesture()
+            .when(UIGestureRecognizer.State.cancelled)
+            .subscribe({ _ in
+                self.cancelRecord()
+                self.recordBtnBackView.backgroundColor = UIColor.white
+            })
+            .disposed(by: disposeBag)
 //        bindViewModel()
     }
     
@@ -37,6 +59,52 @@ class MainViewController: UIViewController {
         recordBackColoredView.cornerRadius = recordBackColoredView.frame.height * 0.5
         recordBtnBackView.cornerRadius = recordBtnBackView.frame.height * 0.5
         recordButton.cornerRadius = recordButton.frame.height * 0.5
+    }
+    
+    private func setupAudio() {
+        recordingSession = AVAudioSession.sharedInstance()
+       
+        // 녹음 기록 불러와서 저장할 제목
+        if let number: Int = UserDefaults.standard.object(forKey: "myNumber") as? Int {
+            numberOfRecords = number
+        }
+        AVAudioSession.sharedInstance().requestRecordPermission({ hasPermission in
+            if hasPermission { print("Accepted!") }
+        })
+    }
+    
+    func directoryOfRecording() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    private func startToRecord() {
+        if audioRecorder == nil {
+            numberOfRecords += 1
+            let filename = directoryOfRecording().appendingPathComponent("\(numberOfRecords).m4a")
+            let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                           AVSampleRateKey: 12000,
+                           AVNumberOfChannelsKey : 1,
+                           AVEncoderAudioQualityKey : AVAudioQuality.high.rawValue]
+            // Start Audio Recording
+            do {
+                audioRecorder = try AVAudioRecorder(url: filename, settings: settings)
+                audioRecorder.delegate = self
+                audioRecorder.record()
+                
+                recordButton.setTitle("Recordiing..", for: .normal)
+            } catch {
+                print("error..!")
+            }
+        }
+    }
+    
+    private func cancelRecord() {
+        // Stop audio recording
+        audioRecorder.stop()
+        audioRecorder = nil
+        UserDefaults.standard.set(numberOfRecords, forKey: "myNumber")
+        recordButton.setTitle("Start Recording", for: .normal)
     }
     
 //
