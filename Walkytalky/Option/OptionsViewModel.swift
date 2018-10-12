5d6352a8b35a2e4b714c65943ecdbb0696c70f3a
ahn.thekan <ahn.thekan@navercorp.com>
2018-10-12 19:31:47 +0900

    Option 틀 작업

---
+++ Walkytalky/Option/OptionsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  OptionsViewModel.swift
+//  Walkytalky
+//
+//  Created by 안덕환 on 12/10/2018.
+//  Copyright © 2018 sutie. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class OptionsViewModel {
+    
+}
