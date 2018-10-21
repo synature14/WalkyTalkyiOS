0d3cd14dc6107648ceeef9f38864fa30520f6286
ahn.thekan <ahn.thekan@navercorp.com>
2018-10-21 23:17:01 +0900

    VoiceRecorder 유틸클래스 추가

---
+++ Walkytalky/Common/VoiceRecorder.swift
@@ -0,0 +1,38 @@
+//
+//  VoiceRecorder.swift
+//  Walkytalky
+//
+//  Created by 안덕환 on 21/10/2018.
+//  Copyright © 2018 sutie. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import RxSwift
+
+class VoiceRecorder {
+    let onVoiceCaptured = PublishSubject<Data>()
+    
+    private lazy var audioEngine: AVAudioEngine = {
+        let audioEngine = AVAudioEngine()
+        let format = audioEngine.inputNode.inputFormat(forBus: 0)
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: format) { [weak self] (buffer, time) in
+            let data = Data(buffer: buffer, time: time)
+            self?.onVoiceCaptured.onNext(data)
+        }
+        return audioEngine
+    }()
+    
+    func startRecording() {
+        do {
+            try audioEngine.start()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func stopRecording() {
+        audioEngine.stop()
+    }
+}
+
